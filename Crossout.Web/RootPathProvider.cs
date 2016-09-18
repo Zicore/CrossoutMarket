b68c67aba91d45f177fe49fc028ac2ce6f8aeb68
Zicore <zicore@gmx.de>
2016-09-18 17:55:32 +0200

    Generalized RootPathProvider

--- Crossout.Web/RootPathProvider.cs
+++ Crossout.Web/RootPathProvider.cs
@@ -1,16 +1,20 @@
 ﻿using System;
 using System.IO;
 using Nancy;
+using NLog;
 
 namespace Crossout.Web
 {
     public class RootPathProvider : IRootPathProvider
     {
+        private static readonly Logger Log = LogManager.GetCurrentClassLogger();
         public string GetRootPath()
         {
-            return StaticConfiguration.IsRunningDebug
+            
+            var path = StaticConfiguration.IsRunningDebug
                 ? Path.GetFullPath(Path.Combine(AppDomain.CurrentDomain.BaseDirectory, "..", ".."))
                 : AppDomain.CurrentDomain.BaseDirectory;
+            return path;
         }
     }
 }
