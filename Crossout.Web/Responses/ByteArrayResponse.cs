50363c78fcf57ddaded85584d645d6b966269af1
Stiffi <git@mxsd.de>
2016-10-21 15:01:06 +0200

    Added embed image creation

---
+++ Crossout.Web/Responses/ByteArrayResponse.cs
@@ -0,0 +1,34 @@
+﻿using Nancy;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Crossout.Web.Responses
+{
+    class ByteArrayResponse : Response
+    {
+        public ByteArrayResponse(byte[] body, string contentType = null)
+        {
+            this.ContentType = contentType ?? "application/octet-stream";
+
+            this.Contents = stream =>
+            {
+                using (var writer = new BinaryWriter(stream))
+                {
+                    writer.Write(body);
+                }
+            };
+        }
+    }
+
+    public static class Extensions
+    {
+        public static Response FromByteArray(this IResponseFormatter formatter, byte[] body, string contentType = null)
+        {
+            return new ByteArrayResponse(body, contentType);
+        }
+    }
+}
