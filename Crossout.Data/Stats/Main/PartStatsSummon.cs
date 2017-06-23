532465bc1a344f34dedf7ec6664859ed557d8b3f
Zicore <zicore@live.de>
2017-06-24 00:56:34 +0200

    remove summon stat properties for now

--- Crossout.Data/Stats/Main/PartStatsSummon.cs
+++ Crossout.Data/Stats/Main/PartStatsSummon.cs
@@ -3,39 +3,10 @@
     public partial class PartStatsSummon : PartStatsBase
     {
         // Custom Properties
-
-        [Stat("Power Score", 0, CustomClasses = StatAttribute.PowerScoreClasses)]
-        public int StatPowerScore
-        {
-            get { return universal_rating; }
-        }
-
-        [Stat("Required Level", 5)]
-        public int StatRequiredLevel
-        {
-            get { return require_faction_level; }
-        }
-
-        //------
-
-        //------
-
         [Stat("Structure", 60)]
         public double StatStructure
         {
             get { return health; }
-        }
-
-        [Stat("Energy Drain", 70)]
-        public int StatEnergyDrain
-        {
-            get { return power_require; }
-        }
-
-        [Stat("Mass", 80)]
-        public double StatMass
-        {
-            get { return physics_mass; }
         }
     }
 }
