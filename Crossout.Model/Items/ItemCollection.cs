92b8a57dddc655d9011f1947166bd31794344e34
Stiffi <git@mxsd.de>
2017-06-29 23:54:11 +0200

    Nancy Preperations

--- Crossout.Model/Items/ItemCollection.cs
+++ Crossout.Model/Items/ItemCollection.cs
@@ -4,7 +4,7 @@ namespace Crossout.Model.Items
 {
     public class ItemCollection
     {
-        public List<Item> Items { get; } = new List<Item>();
+        public List<Item> Items { get; set; } = new List<Item>();
 
         public override string ToString()
         {
---
+++ Crossout.Web/Modules/Search/CompareModule.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Crossout.Model.Items;
+using Crossout.Model.Recipes;
+using Crossout.Web.Models;
+using Crossout.Web.Services;
+using Nancy;
+using ZicoreConnector.Zicore.Connector.Base;
+
+namespace Crossout.Web.Modules.Search
+{
+    public class CompareModule : NancyModule
+    {
+        public CompareModule()
+        {
+            Get["/compare/(?<others>.*)"] = Render;
+        }
+
+        private Response Render(dynamic parameters)
+        {
+            var result = new List<int>();
+            foreach (var other in ((string)parameters.others).Split('|'))
+            {
+                try
+                {
+                    result.Add(Convert.ToInt32((other)));
+                }
+                catch
+                {
+                    return Response.AsRedirect("/");
+                }
+
+            }
+            return RouteCompare(result);
+        }
+
+        SqlConnector sql = new SqlConnector(ConnectionType.MySql);
+
+        private dynamic RouteCompare(List<int> ids)
+        {
+            try
+            {
+
+                RecipeItem.ResetId();
+                sql.Open(WebSettings.Settings.CreateDescription());
+
+                DataService db = new DataService(sql);
+
+                var itemList = new List<Item>();
+
+                foreach (var id in ids)
+                {
+                    var itemModel = db.SelectItem(id, true);
+                    CrossoutDataService.Instance.AddData(itemModel.Item);
+                    itemList.Add(itemModel.Item);
+                }
+                var itemCol = new ItemCollection();
+                itemCol.Items = itemList;
+
+                return View["compare", itemCol];
+            }
+            catch
+            {
+                return Response.AsRedirect("/");
+            }
+        }
+    }
+}
