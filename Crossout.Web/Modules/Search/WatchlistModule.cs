6057f2c4b113cf5d7af40cd4465eb4ce5778c8cc
Zicore <zicore@live.de>
2017-08-27 11:00:44 +0200

    implemented watchlist like compare, refactor compare

---
+++ Crossout.Web/Modules/Search/WatchlistModule.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Crossout.Model.Items;
+using Crossout.Web.Models.General;
+using Crossout.Web.Services;
+using Nancy;
+using ZicoreConnector.Zicore.Connector.Base;
+
+namespace Crossout.Web.Modules.Search
+{
+    public class WatchlistModule : NancyModule
+    {
+        public WatchlistModule()
+        {
+            Get["/watchlist/(?<ids>.*)"] = x =>
+            {
+                return RouteCompare(x);
+            };
+        }
+
+        SqlConnector sql = new SqlConnector(ConnectionType.MySql);
+
+        private dynamic RouteCompare(dynamic items)
+        {
+            var result = new List<int>();
+            var idsString = (string)items.ids;
+
+            var ids = idsString.Split(',');
+
+            foreach (var id in ids)
+            {
+                int foundId;
+                if (int.TryParse(id, out foundId))
+                {
+                    result.Add(foundId);
+                }
+            }
+
+            try
+            {
+                sql.Open(WebSettings.Settings.CreateDescription());
+
+                DataService db = new DataService(sql);
+
+                var itemList = new List<Item>();
+
+                foreach (var id in result)
+                {
+                    var itemModel = db.SelectItem(id, true);
+                    CrossoutDataService.Instance.AddData(itemModel.Item);
+                    if (itemModel.Item.Stats != null)
+                    {
+                        itemList.Add(itemModel.Item);
+                    }
+                }
+                var watchlist = new WatchlistModel();
+                watchlist.Items = itemList;
+
+                var statusModel = db.SelectStatus();
+                watchlist.Status = statusModel;
+
+                return View["watchlist", watchlist];
+            }
+            catch
+            {
+                return Response.AsRedirect("/");
+            }
+        }
+    }
+}
---
+++ Crossout.Web/Scripts/Internal/search.js
@@ -0,0 +1,113 @@
+﻿$(document).ready(function () {
+
+    var domOption =
+        "<'content-space'<'row'<'col-sm-6'p><'col-sm-4'f><'col-sm-2'l>>>" +
+            "tr" +
+            "<'content-space'<'row'<'col-sm-5'p><'col-sm-3'B><'col-sm-2'i><'col-sm-2'l>>>";
+
+    $.fn.DataTable.ext.pager.numbers_length = 10;
+
+    $('#ItemTable').DataTable({
+        paging: true,
+        searching: true,
+        search: {
+            smart: false,
+            regex: true
+        },
+        buttons: {
+            dom: {
+                container: {
+                    tag: 'ul',
+                    className: 'pagination dataTables_button'
+                },
+                buttonContainer: {
+                    tag: 'li',
+                    className: 'paginate_button'
+                },
+                button: {
+                    tag: 'a',
+                    className: ''
+                }
+            },
+            buttons: [{
+                    extend: 'excel',
+                    text: 'Export as Excel',
+                    filename: 'CrossoutDBExcelExport',
+                    exportOptions: {
+                        modifier: {
+                            page: 'current'
+                        }
+                    }
+                }, {
+                    extend: 'csv',
+                    text: 'Export as CSV',
+                    filename: 'CrossoutDBCSVExport',
+                    exportOptions: {
+                        modifier: {
+                            page: 'current'
+                        }
+                    }
+                }, {
+                    action: function () {
+                        compareSelected();
+                    },
+                    text: 'Compare selected'
+                }, {
+                    action: function () {
+                        watchlistSelected();
+                    },
+                    text: 'Watchlist selected'
+                }
+            ]
+        },
+        order: [[4, "asc"]],
+        lengthMenu: [[10, 20, 50, -1], [10, 20, 50, "All"]],
+        pagingType: "full_numbers",
+        dom: domOption
+    });
+});
+
+var selectedList = [];
+var highlightSelectClass = 'info';
+
+$('.selected-row').click(function () {
+    if ($(this).hasClass(highlightSelectClass)) {
+        $(this).removeClass(highlightSelectClass);
+        selectedList.splice(selectedList.findIndex(x => x === $(this).data('id')), 1);
+    } else {
+        $(this).addClass(highlightSelectClass);
+        selectedList.push($(this).data('id'));
+    }
+});
+
+function compareSelected() {
+    if (selectedList.length !== 0) {
+        var url = "/compare/";
+        var i = 0;
+        selectedList.forEach(function (e) {
+            if (i !== 0) {
+                url += ",";
+            }
+            var id = e;
+            url += id;
+            i++;
+        });
+        window.location = url;
+    }
+}
+
+function watchlistSelected() {
+    if (selectedList.length !== 0) {
+        var url = "/watchlist/";
+        var i = 0;
+        selectedList.forEach(function (e) {
+            if (i !== 0) {
+                url += ",";
+            }
+            var id = e;
+            url += id;
+            i++;
+        });
+        window.location = url;
+    }
+}
