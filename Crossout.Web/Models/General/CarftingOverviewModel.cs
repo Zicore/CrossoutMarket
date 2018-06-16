94c30d9c3cb292d0b8ea79bcb98d5942ada7c24b
Stiffi <git@mxsd.de>
2018-06-16 18:19:07 +0200

    Add crafting overview page

---
+++ Crossout.Web/Models/General/CarftingOverviewModel.cs
@@ -0,0 +1,20 @@
+﻿using Crossout.Model.Items;
+using Crossout.Web.Helper;
+using Crossout.Web.Models.Filter;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Crossout.Web.Models.General
+{
+    public class CraftingOverviewModel : IViewTitle
+    {
+        public List<Item> Items { get; set; }
+        public FilterModel FilterModel { get; set; } = new FilterModel();
+        public StatusModel Status { get; set; } = new StatusModel();
+
+        public string Title => null;
+    }
+}
