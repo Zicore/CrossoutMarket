781af576e271f215095612efa70e83b6334b1a99
Stiffi <git@mxsd.de>
2018-07-16 15:11:50 +0200

    Save order and page length in url

--- Crossout.Web/Scripts/Internal/locationhash.js
+++ Crossout.Web/Scripts/Internal/locationhash.js
@@ -1,59 +1,74 @@
 ﻿function applyLocationHash(table) {
     var hash = location.hash;
-    var patttern = '#faction=(.*,?)\.rarity=(.*,?)\.category=(.*,?)\.';
-    var regEx = new RegExp(patttern, 'ig');
-    var matches = regEx.exec(hash);
-    if (matches != null) {
-        matches.forEach(function (match, i) {
-            if (i !== 0) {
-                var items = match.split(',');
+    var pattern = '(?<type>faction|rarity|category|length|order)=(?<items>.*,?)';
+    hash = hash.replace('#', '');
+    var types = hash.split('.');
+    types.forEach(function (type, i) {
+        var regEx = new RegExp(pattern, 'ig');
+        var matches = regEx.exec(type);
+        if (matches != null) {
+            var typeName = matches.groups['type'];
 
-                items.forEach(function (item, j) {
-                    if (i === 1) {
-                        $('.filter-faction').each(function (k, e) {
-                            if (k < $('.filter-faction').toArray().length) {
-                                var targetString = $(this).parent().text().toLowerCase();
-                                targetString = targetString.replace(' ', '');
-                                if (targetString === item) {
-                                    $(this).parent().addClass('active');
-                                }
+            var items = matches.groups['items'].split(',');
+
+            items.forEach(function (item, j) {
+                if (typeName === "faction") {
+                    $('.filter-faction').each(function (k, e) {
+                        if (k < $('.filter-faction').toArray().length) {
+                            var targetString = $(this).parent().text().toLowerCase();
+                            targetString = targetString.replace(' ', '');
+                            if (targetString === item) {
+                                $(this).parent().addClass('active');
                             }
-                        });
-                    } else if (i === 2) {
-                        $('.filter-rarity').each(function (k, e) {
-                            if (k < $('.filter-rarity').toArray().length) {
-                                var targetString = $(this).parent().text().toLowerCase();
-                                targetString = targetString.replace(' ', '');
-                                if (targetString === item) {
-                                    $(this).parent().addClass('active');
-                                }
+                        }
+                    });
+                } else if (typeName === "rarity") {
+                    $('.filter-rarity').each(function (k, e) {
+                        if (k < $('.filter-rarity').toArray().length) {
+                            var targetString = $(this).parent().text().toLowerCase();
+                            targetString = targetString.replace(' ', '');
+                            if (targetString === item) {
+                                $(this).parent().addClass('active');
                             }
-                        });
-                    } else if (i === 3) {
-                        $('.filter-category').each(function (k, e) {
-                            if (k < $('.filter-category').toArray().length) {
-                                var targetString = $(this).parent().text().toLowerCase();
-                                targetString = targetString.replace(' ', '');
-                                if (targetString === item) {
-                                    $(this).parent().addClass('active');
-                                }
+                        }
+                    });
+                } else if (typeName === "category") {
+                    $('.filter-category').each(function (k, e) {
+                        if (k < $('.filter-category').toArray().length) {
+                            var targetString = $(this).parent().text().toLowerCase();
+                            targetString = targetString.replace(' ', '');
+                            if (targetString === item) {
+                                $(this).parent().addClass('active');
                             }
-                        });
+                        }
+                    });
+                } else if (typeName === "length") {
+                    table.page.len(item).draw();
+                } 
+                else if (typeName === "order") {
+                    if (item.includes('asc')) {
+                        var columnNumber = item.replace('asc', '');
+                        table.order([columnNumber, 'asc']);
+                    } else if (item.includes('desc')) {
+                        var columnNumber = item.replace('desc', '');
+                        table.order([columnNumber, 'desc']);
                     }
-                });
-            }
-        });
-        filterTable(table);
-    }
+                }
+            });
+            filterTable(table);
+        }
+    }); 
 }
 
-function updateLocationHash() {
+function updateLocationHash(table) {
     var newHash = '#';
 
-    newHash += 'faction=';
     $('.filter-faction').each(function (k, e) {
         if (k < $('.filter-faction').toArray().length / 2) {
             if ($(this).parent().hasClass('active')) {
+                if (!newHash.includes('faction=')) {
+                    newHash += 'faction=';
+                }
                 var targetString = $(this).parent().text().toLowerCase();
                 targetString = targetString.replace(' ', '');
                 newHash += targetString + ',';
@@ -63,13 +78,16 @@ function updateLocationHash() {
     if (newHash.endsWith(',')) {
         newHash = newHash.substr(0, newHash.length - 1);
     }
-    newHash += '.';
-
-    newHash += 'rarity=';
+    if (newHash.includes('faction=')) {
+        newHash += '.';
+    }
 
     $('.filter-rarity').each(function (k, e) {
         if (k < $('.filter-rarity').toArray().length / 2) {
             if ($(this).parent().hasClass('active')) {
+                if (!newHash.includes('rarity=')) {
+                    newHash += 'rarity=';
+                }
                 var targetString = $(this).parent().text().toLowerCase();
                 targetString = targetString.replace(' ', '');
                 newHash += targetString + ',';
@@ -79,13 +97,16 @@ function updateLocationHash() {
     if (newHash.endsWith(',')) {
         newHash = newHash.substr(0, newHash.length - 1);
     }
-    newHash += '.';
-
-    newHash += 'category=';
+    if (newHash.includes('rarity=')) {
+        newHash += '.';
+    }
 
     $('.filter-category').each(function (k, e) {
         if (k < $('.filter-category').toArray().length / 2) {
             if ($(this).parent().hasClass('active')) {
+                if (!newHash.includes('category=')) {
+                    newHash += 'category=';
+                }
                 var targetString = $(this).parent().text().toLowerCase();
                 targetString = targetString.replace(' ', '');
                 newHash += targetString + ',';
@@ -95,6 +116,41 @@ function updateLocationHash() {
     if (newHash.endsWith(',')) {
         newHash = newHash.substr(0, newHash.length - 1);
     }
-    newHash += '.';
+    if (newHash.includes('category=')) {
+        newHash += '.';
+    }
+
+    if (table.page.info().length !== $.fn.DataTable.ext.pager.numbers_length) {
+        if (!newHash.includes('length=')) {
+            newHash += 'length=';
+        }
+        newHash += table.page.info().length;
+    }
+    if (newHash.includes('length=')) {
+        newHash += '.';
+    }
+
+    var newOrder = table.order();
+    if (newOrder[0].toString() != defaultOrder[0].toString() ) {
+        if (!newHash.includes('order=')) {
+            newHash += 'order=';
+        }
+        newHash += newOrder[0].toString().replace(',', '');
+    }
+    if (newHash.includes('order=')) {
+        newHash += '.';
+    }
+
     location.hash = newHash;
 }
+
+$('#ItemTable').on('length.dt', function (e, settings, len) {
+    updateLocationHash($('#ItemTable').DataTable());
+});
+
+$('#ItemTable').on('order.dt', function () {
+    var newOrder = $('#ItemTable').DataTable().order();
+    if (newOrder[0].toString() != defaultOrder[0].toString()) {
+        updateLocationHash($('#ItemTable').DataTable());
+    }
+});
