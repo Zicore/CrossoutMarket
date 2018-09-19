67f020991e438e4f04c459498e262f848dd35e3e
Stiffi <git@mxsd.de>
2018-09-19 17:06:41 +0200

    Add event page

---
+++ Crossout.Data/KnightRiders/KnightRiders.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Crossout.Data.KnightRiders
+{
+    public class Ingredient
+    {
+        public string Name;
+        public int Id;
+        public int Amount;
+        public decimal SellPrice;
+        public decimal BuyPrice;
+        public string FormatSellPrice;
+        public string FormatBuyPrice;
+    }
+
+    public class EventItem
+    {
+        public string Key;
+        public string Name;
+        public int? Id;
+        public List<Ingredient> Ingredients;
+        public string FormatSellSum;
+        public string FormatBuySum;
+        public decimal TotalSellSum;
+        public decimal TotalBuySum;
+        public string FormatTotalSellSum;
+        public string FormatTotalBuySum;
+    }
+}
