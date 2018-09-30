7fdc4286a840c8989d6681dd29b7e329712ec36f
Stiffi <git@mxsd.de>
2018-09-30 18:38:24 +0200

    Update stats to newest version

--- Crossout.Data/Stats/Main/PartStatsMelee.Generated.cs
+++ Crossout.Data/Stats/Main/PartStatsMelee.Generated.cs
@@ -5,8 +5,10 @@ namespace Crossout.Data.Stats.Main
     {
         public PartStatsMelee(string key) : base(key) { }
         public bool ods_export {get;set;}
+        public string inherit {get;set;}
         public string @class {get;set;}
         public bool released {get;set;}
+        public bool tradeable {get;set;}
         public string item_type {get;set;}
         public string subtype {get;set;}
         public string model {get;set;}
@@ -15,42 +17,32 @@ namespace Crossout.Data.Stats.Main
         public string taglist {get;set;}
         public string ui_part_type_icon {get;set;}
         public string ui_part_type {get;set;}
+        public string ui_hud_icon {get;set;}
         public int health {get;set;}
         public int collision_resist {get;set;}
+        public bool individual_collision {get;set;}
         public bool damageable {get;set;}
         public int rarity {get;set;}
+        public string require_faction {get;set;}
+        public int require_faction_level {get;set;}
         public double collision_damage {get;set;}
         public string physics_material {get;set;}
         public int physics_mass {get;set;}
         public int universal_rating {get;set;}
-        public double damage_rating {get;set;}
-        public string ui_part_series {get;set;}
-        public double damage_transmission {get;set;}
-        public string inherit {get;set;}
-        public bool tradeable {get;set;}
-        public string ui_hud_icon {get;set;}
-        public bool individual_collision {get;set;}
-        public int require_faction_level {get;set;}
         public bool important {get;set;}
+        public string perk {get;set;}
         public string fusion {get;set;}
         public string fusion_cost {get;set;}
         public int power_require {get;set;}
         public double saw_damage {get;set;}
+        public bool activate_click {get;set;}
+        public int cooldown {get;set;}
+        public int work_time {get;set;}
+        public double damage_rating {get;set;}
         public bool salvageable {get;set;}
         public int salvage_Scrap_Common {get;set;}
         public int salvage_Scrap_Rare {get;set;}
         public int salvage_Scrap_Epic {get;set;}
-        public string require_faction {get;set;}
-        public string perk {get;set;}
-        public double detonation_radius {get;set;}
-        public int blast_damage {get;set;}
-        public int blast_damage_experimental {get;set;}
-        public int blast_radius {get;set;}
-        public int blast_radius_experimental {get;set;}
-        public int blast_impulse {get;set;}
-        public bool activate_click {get;set;}
-        public int cooldown {get;set;}
-        public int work_time {get;set;}
         public bool activate_hold {get;set;}
         public int heat_inc {get;set;}
         public int heat_max {get;set;}
--- Crossout.Data/Stats/Main/PartStatsStructure.Generated.cs
+++ Crossout.Data/Stats/Main/PartStatsStructure.Generated.cs
@@ -7,6 +7,7 @@ namespace Crossout.Data.Stats.Main
         public bool ods_export {get;set;}
         public string @class {get;set;}
         public bool released {get;set;}
+        public string xopedia_keyword {get;set;}
         public string item_type {get;set;}
         public string model {get;set;}
         public string editor_type {get;set;}
@@ -15,14 +16,17 @@ namespace Crossout.Data.Stats.Main
         public string ui_part_type_icon {get;set;}
         public string ui_part_type {get;set;}
         public int health {get;set;}
-        public bool damageable {get;set;}
         public int rarity {get;set;}
         public double damage_transmission {get;set;}
         public string physics_material {get;set;}
         public int physics_mass {get;set;}
         public int universal_rating {get;set;}
         public int car_health {get;set;}
-        public bool tradeable {get;set;}
+        public string subtype {get;set;}
+        public int collision_resist {get;set;}
+        public bool count_stats {get;set;}
+        public double collision_damage {get;set;}
+        public double damage_rating {get;set;}
         public bool store_in_inventory {get;set;}
     }
 }
