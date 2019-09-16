b9e53d92b373e99b75b8448ff8c970a09f21adc0
Zicore <zicore@live.de>
2019-09-16 17:08:10 +0200

    fix bugs, add ads.txt, refactoring

--- Crossout.Web/Models/Charts/ChartDataModel.cs
+++ Crossout.Web/Models/Charts/ChartDataModel.cs
@@ -11,73 +11,38 @@ namespace Crossout.Web.Models.Charts
         public string Name { get; set; }
         public int Id { get; set; }
 
-        public object[][] Data
+        public object[][][] Data
         {
             get
             {
-                if (Name == "all")
+                var data = new object[4][][];
+                for (int i = 0; i < 4; i++)
                 {
-                    var data = new object[4][][];
-
-                    for (int i = 0; i < 4; i++)
+                    data[i] = new object[Items.Count][];
+                    for (int j = 0; j < Items.Count; j++)
                     {
-                        data[i] = new object[Items.Count][];
-                        for(int j = 0; j < Items.Count; j++)
+                        data[i][j] = new object[2];
+                        data[i][j][0] = Items[j].UnixTimestamp;
+                        if (i == 0)
                         {
-                            data[i][j] = new object[2];
-                            data[i][j][0] = Items[j].UnixTimestamp;
-                            if (i == 0)
-                            {
-                                data[i][j][1] = Items[j].FormatSellPrice;
-                            }
-                            else if (i == 1)
-                            {
-                                data[i][j][1] = Items[j].FormatBuyPrice;
-                            }
-                            else if (i == 2)
-                            {
-                                data[i][j][1] = Items[j].SellOffers;
-                            }
-                            else if (i == 3)
-                            {
-                                data[i][j][1] = Items[j].BuyOrders;
-                            }
-
+                            data[i][j][1] = Items[j].FormatSellPrice;
                         }
+                        else if (i == 1)
+                        {
+                            data[i][j][1] = Items[j].FormatBuyPrice;
+                        }
+                        else if (i == 2)
+                        {
+                            data[i][j][1] = Items[j].SellOffers;
+                        }
+                        else if (i == 3)
+                        {
+                            data[i][j][1] = Items[j].BuyOrders;
+                        }
+
                     }
-
-                    return data;
                 }
-                else
-                {
-                    var data = new object[Items.Count][];
-
-                    for (int i = 0; i < Items.Count; i++)
-                    {
-                        object[] row = new object[2];
-                        row[0] = Items[i].UnixTimestamp;
-                        if (Name == "sell")
-                        {
-                            row[1] = Items[i].FormatSellPrice;
-                        }
-                        else if (Name == "buy")
-                        {
-                            row[1] = Items[i].FormatBuyPrice;
-                        }
-                        else if (Name == "selloffers")
-                        {
-                            row[1] = Items[i].SellOffers;
-                        }
-                        else if (Name == "buyorders")
-                        {
-                            row[1] = Items[i].BuyOrders;
-                        }
-                        data[i] = row;
-                    }
-
-                    return data;
-                }
-
+                return data;
             }
         }
 
