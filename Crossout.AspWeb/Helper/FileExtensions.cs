47020a1200b4ccdbc3d755dd1288c524d8847edf
Zicore <zicore@live.de>
2019-10-16 17:43:57 +0200

    refactor usage of RootPathHelper

---
+++ Crossout.AspWeb/Helper/FileExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Crossout.Model.Items;
+using Crossout.Web;
+
+namespace Crossout.AspWeb.Helper
+{
+    public static class FileExtensions
+    {
+        public static void SetImageExists(this Item item, RootPathHelper rootPathHelper)
+        {
+            item.ImageExists = rootPathHelper.ImageExists(item.Image);
+            item.HighResImageExists = rootPathHelper.HighResImageExists(item.Image);
+        }
+    }
+}
