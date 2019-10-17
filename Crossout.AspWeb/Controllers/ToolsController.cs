91449619115a90ef27ff7e6e2191cd57b57e3df4
Stiffi <git@mxsd.de>
2019-10-17 10:10:06 +0200

    Add ToolsController and View

---
+++ Crossout.AspWeb/Controllers/ToolsController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Crossout.Web.Models.General;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Crossout.AspWeb.Controllers
+{
+    public class ToolsController : Controller
+    {
+        [Route("tools")]
+        public IActionResult Tools()
+        {
+            ToolsModel model = new ToolsModel();
+            return View("tools", model);
+        }
+    }
+}
