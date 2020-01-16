ee88f125297560f118ccac5da6d42cae3fa35bd7
Stiffi <git@mxsd.de>
2020-01-16 20:47:11 +0100

    Port worker to .NET Core

---
+++ Crossout.WorkerCore/TaskSettings.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Crossout.WorkerCore
+{
+    public class TaskSettings
+    {
+        public string Key;
+        public string ExecutionInterval;
+        public string ExecutionTime;
+    }
+}
