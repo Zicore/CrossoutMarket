c557b12e6819da215bef45324939df4f64e2afc8
Stiffi <git@mxsd.de>
2020-01-20 07:19:25 +0100

    Add cache busting helper class

---
+++ Crossout.AspWeb/Helper/CacheBuster.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Crossout.AspWeb.Helper
+{
+    public static class CacheBuster
+    {
+        public static string Seconds { get => "t=" + DateTime.UtcNow.Subtract(new DateTime(1970, 1, 1, 0, 0, 0)).TotalSeconds.ToString("0"); }
+        public static string Hours { get => "t=" + DateTime.UtcNow.Subtract(new DateTime(1970, 1, 1, 0, 0, 0)).TotalHours.ToString("0"); }
+    }
+}
