72b29312577f50afeb304c1a18eca05b64b104a9
Stiffi <git@mxsd.de>
2020-01-22 12:07:57 +0100

    Add base for api v2

---
+++ Crossout.AspWeb/Models/API/v2/MarketAllRequest.cs
@@ -0,0 +1,11 @@
+﻿namespace Crossout.Web.Modules.API.v2
+{
+    public class MarketAllRequest
+    {
+        public int Id { get; set; }
+
+        public int? StartTimestamp { get; set; }
+
+        public int? EndTimestamp { get; set; }
+    }
+}
