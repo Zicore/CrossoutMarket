292fa248bc86919a3700d61f631dce0fcbf50c17
Stiffi <git@mxsd.de>
2020-01-29 10:45:09 +0100

    Add method blacklist to hit register

--- Crossout.AspWeb/Helper/ControllerExtensions.cs
+++ Crossout.AspWeb/Helper/ControllerExtensions.cs
@@ -4,25 +4,30 @@ using Microsoft.AspNetCore.Mvc;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Threading.Tasks;
 
 namespace Crossout.AspWeb.Helper
 {
     public static class ControllerExtensions
     {
+        public static HashSet<string> DoNotRegisterMethodList = new HashSet<string>{ "OPTIONS" };
+
         public static void RegisterHit(this Controller controller, string displayName, int? idParameter = null)
         {
-            Hit hit = new Hit();
-            hit.ActionId = controller.ControllerContext.ActionDescriptor.Id;
-            hit.ActionName = controller.ControllerContext.ActionDescriptor.ActionName;
-            hit.ControllerName = controller.ControllerContext.ActionDescriptor.ControllerName;
-            hit.IPHash = controller.HttpContext.Connection.RemoteIpAddress.GetHashCode();
-            hit.IdParameter = idParameter;
-            hit.HitTimestamp = DateTime.UtcNow;
-            hit.ActionDisplayName = displayName;
+            if (!DoNotRegisterMethodList.Contains(controller.HttpContext.Request.Method))
+            {
+                Hit hit = new Hit();
+                hit.ActionId = controller.ControllerContext.ActionDescriptor.Id;
+                hit.ActionName = controller.ControllerContext.ActionDescriptor.ActionName;
+                hit.ControllerName = controller.ControllerContext.ActionDescriptor.ControllerName;
+                hit.IPHash = controller.HttpContext.Connection.RemoteIpAddress.GetHashCode();
+                hit.IdParameter = idParameter;
+                hit.HitTimestamp = DateTime.UtcNow;
+                hit.ActionDisplayName = displayName;
 
-            StatsService.Instance.AddHit(hit);
-            Console.WriteLine($"Registered Hit by {hit.IPHash} using Controller {hit.ControllerName} executing Action {hit.ActionName} with ID {hit.ActionId}. IdParameter is {hit.IdParameter}");
+                StatsService.Instance.AddHit(hit);
+            }
         }
     }
 }
