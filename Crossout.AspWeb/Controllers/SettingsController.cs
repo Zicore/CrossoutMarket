831c0cb852612ab49afc7e2f88650fca757aa1e6
Stiffi <git@mxsd.de>
2020-02-05 04:21:34 +0100

    Generate language selector from available languages

--- Crossout.AspWeb/Controllers/SettingsController.cs
+++ Crossout.AspWeb/Controllers/SettingsController.cs
@@ -4,17 +4,28 @@ using System.Linq;
 using System.Threading.Tasks;
 using Crossout.AspWeb.Helper;
 using Crossout.AspWeb.Models.General;
+using Crossout.AspWeb.Models.Language;
 using Crossout.AspWeb.Services;
 using Microsoft.AspNetCore.Mvc;
+using ZicoreConnector.Zicore.Connector.Base;
 
 namespace Crossout.AspWeb.Controllers
 {
     public class SettingsController : Controller
     {
+        SqlConnector sql = new SqlConnector(ConnectionType.MySql);
+
         [Route("settings")]
         public IActionResult Settings()
         {
+            sql.Open(WebSettings.Settings.CreateDescription());
+
+            DataService db = new DataService(sql);
+
+            LanguageModel languageModel = db.SelectLanguageModel();
+
             SettingsModel model = new SettingsModel();
+            model.LanguageModel = languageModel;
 
             this.RegisterHit("Settings");
             return View("settings", model);
